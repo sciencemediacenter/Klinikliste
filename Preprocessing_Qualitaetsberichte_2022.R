2f7f62946a47f40fdd1f0c0a90447750d63bdb9b
lkoppersSMC <lars.koppers@sciencemediacenter.de>
2024-05-27 17:20:17 +0200

    Fallzahlen Prozeduren aus den Qualitätsberichten
    Aufräumen des übrigen Codes für die Qualitätsberichte

--- Preprocessing_Qualitaetsberichte_2022.R
+++ Preprocessing_Qualitaetsberichte_2022.R
@@ -1,20 +1,41 @@
 
 # Setup -----------------------------------------------------------
+library(tidyverse)
 library(stringr)
-library(dplyr)
 library(xml2)
 library(pbapply)
+
+## workspace directory und filename
+if (rstudioapi::isAvailable()) {
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+}
+
 source("utils.R")
 
-xml_files <- get_qb_xml_fils()
-# file_path <- xml_files[grepl("260102343-773448000", xml_files)]
+xml_files <-
+  get_qb_xml_files(
+    file_path = file.path("Qualitaetsberichte", "nobackup", "xml_2022"),
+    pattern = "2022-xml\\.xml$"
+  )
 
 # Lese Qualitätsberichte ein --------------------------------------
 qualitaetsdaten <- pblapply(xml_files, read_qualitaetsberichte_xml)
-
 qualitaetsdaten <- bind_rows(qualitaetsdaten)
 
 # Save data -------------------------------------------------------
 save(qualitaetsdaten,
      file = file.path("Qualitaetsberichte", "Qualitaetsdaten.Rdata"))
 
+
+
+# Lese Prozeduren ein ---------------------------------------------
+
+Fallzahlen_Prozeduren <- pblapply(xml_files, read_qualitaetsberichte_xml_prozeduren)
+Fallzahlen_Prozeduren <- 
+  bind_rows(Fallzahlen_Prozeduren) |> 
+  select(-Anzahl_Datenschutz) |> 
+  mutate(Anzahl = as.numeric(Anzahl))
+
+# Save data -------------------------------------------------------
+save(Fallzahlen_Prozeduren,
+     file = file.path("Qualitaetsberichte", "Fallzahlen_Prozeduren.Rdata"))
--- utils.R
+++ utils.R
@@ -5,8 +5,6 @@
 #' @param debugmode boolean, Im Debugmode wird der Dateiname zusätzlich
 #' ausgeben, um ggf. manuell die Datenextraktion zu überprüfen.
 #' Default ist FALSE
-#' @param force_valid boolean, Offensichtlich nicht valide Daten, werden
-#' nicht extrahiert. Siehe details. Default ist TRUE
 #' @details
 #' Einige Angaben für die Teilnahme an der Notfallversorgung erscheinen
 #' nicht plausibel bzw. sind anscheinend nicht vollständig in den
@@ -23,54 +21,73 @@
 #'  qualitaetsdaten <- bind_rows(qualitaetsdaten_swr)
 #' }
 
-read_qualitaetsberichte_xml <- function(file_path, debugmode = F,
-                                        force_valid = T) {
-
-  xml_data <- read_xml(file_path)
-
+read_qualitaetsberichte_xml <-
+  function(
+    file_path,
+    debugmode = FALSE) {
+    xml_data <- read_xml(file_path)
+    
   # Gibt es mehrere Standorte?
-  mehrere_standorte <- length(xml_children(xml_find_all(xml_data, "//Krankenhaus/Mehrere_Standorte"))) == 2
-  kh_path <- ifelse(mehrere_standorte, "Standortkontaktdaten", "Krankenhauskontaktdaten")
+    mehrere_standorte <- length(xml_children(xml_find_all(xml_data, "//Krankenhaus/Mehrere_Standorte"))) == 2
+    kh_path <- ifelse(mehrere_standorte, "Standortkontaktdaten", "Krankenhauskontaktdaten")
 
-  ik <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/IK")))
-  name <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Name")))
-  strasse <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Strasse")))
-  hausnummer <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Hausnummer")))
-  plz <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Postleitzahl")))
-  ort <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Ort")))
-  standortnummer <- xml_text(xml_find_all(xml_data, glue::glue("//Standortnummer")))
-  betten <- xml_text(xml_find_all(xml_data, "//Anzahl_Betten"))
-  kommentar_notfallstufe <- xml_text(xml_find_all(xml_data, "//Teilnahme_Notfallstufe/Erlaeuterungen"))
+    ik <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/IK")))
+    name <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Name")))
+    strasse <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Strasse")))
+    hausnummer <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Hausnummer")))
+    plz <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Postleitzahl")))
+    ort <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/Kontakt_Zugang/Ort")))
+    standortnummer <- xml_text(xml_find_all(xml_data, glue::glue("//Standortnummer")))
+    betten <- xml_text(xml_find_all(xml_data, "//Anzahl_Betten"))
+    kommentar_notfallstufe <- xml_text(xml_find_all(xml_data, "//Teilnahme_Notfallstufe/Erlaeuterungen"))
 
-  file <- basename((file_path))
-
-  if (length(kommentar_notfallstufe) == 0)
-    kommentar_notfallstufe <- NA
-
-  # Default Notfallstufe 0
-  notfallstufe <- 0
-
-  if (length(xml_children(xml_find_all(xml_data, "//Basisnotfallversorgung_Stufe_1"))) > 0)
-    notfallstufe <- 1
-
-  if (length(xml_children(xml_find_all(xml_data, "//Erweiterte_Notfallversorgung_Stufe_2"))) > 0)
-    notfallstufe <- 2
-
-  if (length(xml_children(xml_find_all(xml_data, "//Umfassende_Notfallversorgung_Stufe_3"))) > 0)
-    notfallstufe <- 3
-
-  spezialversorgung <- xml_text(xml_find_all(xml_data, "//Module_Spezielle_Notfallversorgung"))
-
-  spezialversorgung <- get_spezialversorgung_code(spezialversorgung)
-
-  data <- data.frame(ik, name, strasse, hausnummer, plz, ort, standortnummer, betten, notfallstufe, kommentar_notfallstufe) %>%
-    bind_cols(spezialversorgung)
-
-  if (debugmode)
-    data <- dplyr::bind_cols(data, file = file)
-
-  return(data)
-}
+    file <- basename((file_path))
+    
+    if (length(kommentar_notfallstufe) == 0) {
+      kommentar_notfallstufe <- NA
+    }
+    
+    # Default Notfallstufe 0
+    notfallstufe <- 0
+    
+    if (length(xml_children(xml_find_all(xml_data, "//Basisnotfallversorgung_Stufe_1"))) > 0){
+      notfallstufe <- 1
+    }
+    
+    if (length(xml_children(xml_find_all(xml_data, "//Erweiterte_Notfallversorgung_Stufe_2"))) > 0){
+      notfallstufe <- 2
+    }
+    
+    if (length(xml_children(xml_find_all(xml_data, "//Umfassende_Notfallversorgung_Stufe_3"))) > 0){
+      notfallstufe <- 3
+    }
+    
+    spezialversorgung <- xml_text(xml_find_all(xml_data, "//Module_Spezielle_Notfallversorgung"))
+    
+    spezialversorgung <- get_spezialversorgung_code(spezialversorgung)
+    
+    ### Daten zusammenfuehren
+    
+    data <-
+      data.frame(
+        ik,
+        name,
+        strasse,
+        hausnummer,
+        plz,
+        ort,
+        standortnummer,
+        betten,
+        notfallstufe,
+        kommentar_notfallstufe
+      ) |>
+      bind_cols(spezialversorgung)
+    
+    if (debugmode)
+      data <- dplyr::bind_cols(data, file = file)
+    
+    return(data)
+  }
 
 
 #' get_qb_xml_fils
@@ -79,15 +96,11 @@ read_qualitaetsberichte_xml <- function(file_path, debugmode = F,
 #'
 #' @return Vektor
 
-get_qb_xml_fils <- function(){
-  directory <- file.path("Qualitaetsberichte", "nobackup", "xml_2022")
-  # Liste aller XML-Dateien im Verzeichnis
-  xml_files <- list.files(directory, pattern = "\\.xml$", full.names = TRUE)
-  xml_files <- xml_files[grepl("2022-xml", xml_files)]
-
-  return(xml_files)
-
-}
+get_qb_xml_files <-
+  function(file_path, pattern = "\\.xml$") {
+    xml_files <- list.files(file_path, pattern = pattern, full.names = TRUE)
+    return(xml_files)
+  }
 
 #' get_spezialversorgung_code
 #'
@@ -111,31 +124,78 @@ get_qb_xml_fils <- function(){
 #'
 #'
 
-get_spezialversorgung_code <- function(codes) {
-  codes <- tolower(codes)
-  alle_spalten <- paste0("sn", sprintf("%02d", 1:6))
+get_spezialversorgung_code <-
+  function(codes) {
+    codes <- tolower(codes)
+    alle_spalten <- paste0("sn", sprintf("%02d", 1:6))
+    
+    # DataFrame erstellen
+    df <- data.frame(matrix(FALSE, nrow = 1, ncol = 6))
+    colnames(df) <- alle_spalten
+    
+    # TRUE setzen für die Spalten, die im Vektor sind
+    df[, codes] <- TRUE
+    
+    # definiere Kinderversorgung
+    kinder_versorgung <-
+      c("sn01" = "basis",
+        "sn02" = "erweitert",
+        "sn03" = "umfassend")
+    df$notfallversorgung_kinder <- NA
+    if (length(kinder_versorgung[df[, names(kinder_versorgung)] |> unlist()] |> unname()) > 0) {
+      df$notfallversorgung_kinder <- 
+        paste(kinder_versorgung[df[, names(kinder_versorgung)] |> unlist()] |> unname(), collapse = ", ")
+    }
+    
+    df <- 
+      df |>
+      select(
+        schwerverletztenversorgung = sn04,
+        stroke_unit = sn05,
+        chest_pain_unit = sn06,
+        notfallversorgung_kinder
+      )
+    
+    return(df)
+    
+  }
 
-  # DataFrame erstellen
-  df <- data.frame(matrix(FALSE, nrow = 1, ncol = 6))
-  colnames(df) <- alle_spalten
 
-  # TRUE setzen für die Spalten, die im Vektor sind
-  df[, codes] <- TRUE
+#' read_qualitaetsberichte_xml_prozeduren
+#' Funktion zum Einlesen und Verarbeiten der XML-Qualitätsberichte zwecks Extraktion der Fallzahlen der Prozeduren
+#' @param file_path string, Pfad zu einer XML-Datei
+#' @return dataframe mit den Fallzahlen für verschiedene Prozeduren nach Standort
+#' @example \dontrun{
+#' xml_files <- list.files(directory, pattern = "\\.xml$", full.names = TRUE)
+#' xml_files <- xml_files[grepl("2022-xml", xml_files)]
+#' Fallzahlen_Prozeduren <- pblapply(xml_files, read_qualitaetsberichte_xml_prozeduren)
+#' }
 
-  # definiere Kinderversorgung
-  kinder_versorgung <- c("sn01" = "basis", "sn02" = "erweitert", "sn03" = "umfassend")
-  df$notfallversorgung_kinder <- NA
-  if (length(kinder_versorgung[df[, names(kinder_versorgung)] %>% unlist()] %>% unname()) > 0)
-    df$notfallversorgung_kinder <- paste(kinder_versorgung[df[, names(kinder_versorgung)] %>% unlist()] %>% unname(), collapse = ", ")
+read_qualitaetsberichte_xml_prozeduren <- 
+  function(file_path){
+    xml_data <- read_xml(file_path)
+    mehrere_standorte <- length(xml_children(xml_find_all(xml_data, "//Krankenhaus/Mehrere_Standorte"))) == 2
+    kh_path <- ifelse(mehrere_standorte, "Standortkontaktdaten", "Krankenhauskontaktdaten")
+    
+    IK <- xml_text(xml_find_all(xml_data, glue::glue("//{kh_path}/IK")))
+    Standortnummer <- xml_text(xml_find_all(xml_data, glue::glue("//Standortnummer")))
+    
+    prozedur <- xml_find_all(xml_data, "//Prozedur")
+    tmp <- lapply(prozedur, xml_children)
+    tmp <- lapply(tmp, function(x) tibble(!!!setNames(xml_text(x), xml_name(x))))
+    
+    prozedurliste <- bind_cols(
+      IK = IK,
+      Standortnummer = Standortnummer,
+      bind_rows(tmp)
+    )
+    
+    return(prozedurliste)
+  }
 
 
-  df <- df %>%
-    select(schwerverletztenversorgung = sn04,
-           stroke_unit = sn05,
-           chest_pain_unit = sn06,
-           notfallversorgung_kinder)
 
-  return(df)
 
-}
+
+
 
